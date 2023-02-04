b283536d922a9fbb5fac65c5226eb1bef4197945
Matthew Pelnik <matthew.pelnik@icloud.com>
2023-02-04 16:00:20 -0600

    updated newer fs statement

--- server.cjs
+++ server.cjs
@@ -1,7 +1,7 @@
 const express = require('express');
 const https = require('https');
 const http = require('http');
-const fs = require('node:fs');
+const fs = require('fs');
 
 const app = express();
 const PORT = 3000;
